342b48e72bb99777bde80dd8202d52a33b204763
Trevin Small <trevincub03@gmail.com>
2022-03-07 16:40:12 -0500

    Finally improved printing of names completed

--- functions.py
+++ functions.py
@@ -2,70 +2,65 @@ import time, keyboard, random, sys
 import lxml
 from bs4 import BeautifulSoup
 
+startDate = ""
+endDate = ""
+
+def setDates(start, end):
+    global startDate
+    global endDate
+    startDate = start
+    endDate = end
+
 def wait(x = 1):
     randTime = random.uniform(0,0.75)
     time.sleep((0.25 + randTime) * x)
 
 def getAgencies(user, titleAgencies, windows, selenBrowser):
     agencyList = []
- 
-    selenBrowser.get('https://rightdata.driggstitle.com/')
-    wait(15)
-    selenBrowser.find_element_by_xpath('//a[@class="l-header-menu-projects bgcheck  no-barba login"]').click()
-    wait()
-    username = selenBrowser.find_element_by_id("name")
-    wait()
-    username.send_keys(user.get_username())
-    wait()
-    password = selenBrowser.find_element_by_id("password")
-    wait()
-    password.send_keys(user.get_password())
-    wait()
-    submit = selenBrowser.find_element_by_class_name("Submit")
-    wait()
-    submit.click()
-    wait()
+
+    selenBrowser.get('https://rightdata.driggstitle.com/prv/site.new_confirm?name=' + user.get_username() + '&password=' + user.get_password())
+    wait(3)
     selenBrowser.get('https://rightdata.driggstitle.com/prv/mta_dta.title_comps')
     wait()
     html = selenBrowser.page_source
     soup = BeautifulSoup(html, 'lxml')
     agencyList = soup.find('table')
     agencyList = agencyList.find_all('input', {'size':'100'})
- 
+
     for i in range(len(agencyList)):
- 
+
         currentAgency = agencyList[i]['value']
- 
+
         if '\xa0' in currentAgency or 'value="293"' in currentAgency or "'" in currentAgency:
- 
+
             currentAgency = currentAgency.replace(u'\xa0', u' ')
             currentAgency = currentAgency.replace(u'value="293">', u'')
             currentAgency = currentAgency.replace(u"'", u'')
- 
+
         if currentAgency != '':
- 
+
             titleAgencies.add_long_agency(currentAgency.lower())
             titleAgencies.add_short_agency(currentAgency[:5].lower())
- 
+
     print(titleAgencies.get_title_agencies())
 
     selenBrowser.get('https://rightdata.driggstitle.com/prv/mta_dta.file_mta_rpt')
     windows.switchWindows(0)
     windows.addWindowToDict(0, 'driggsAgentList')
-    openMta(windows, selenBrowser)
+    openMta(user, windows, selenBrowser)
     windows.switchWindowByDict('driggsAgentList')
- 
-def openMta(windows, selenBrowser):
-     
+
+def openMta(user, windows, selenBrowser):
+
     selenBrowser.execute_script("window.open('about:blank', 'secondtab');")
     selenBrowser.switch_to.window("secondtab")
     windows.updateWindows()
-    wait(2)
+    wait()
     selenBrowser.get("https://www.mymta.com/")
     wait(3)
-    selenBrowser.find_element_by_name("user").send_keys("LornaPhillipy")
+    selenBrowser.find_element_by_name("user").send_keys(user.get_mta_username())
     wait()
-    selenBrowser.find_element_by_name("pw").send_keys("$3scotlanD6")
+    selenBrowser.find_element_by_name("pw").send_keys(user.get_mta_password())
     wait()
     selenBrowser.find_element_by_name("button").click()
     wait()
@@ -76,10 +71,9 @@ def openMta(windows, selenBrowser):
     windows.addWindowToDict(1, 'mtaSearch')
 
 def enterData(agent, titleAgencies, windows, selenBrowser):
-    
-    startDate = "10/31/2020"
-    endDate = "10/31/2021"
-    
+    global startDate
+    global endDate
+
     skippedName = enterDataForDate(agent, titleAgencies, windows, selenBrowser, startDate, endDate, True)
     if skippedName:
         return False
@@ -92,18 +86,18 @@ def enterData(agent, titleAgencies, windows, selenBrowser):
 
     windows.switchWindowByDict('agentPageOne')
     selenBrowser.close()
-    wait(2)
+    wait()
     windows.switchWindowByDict('agentPageTwo')
     selenBrowser.close()
-    wait(2)
+    wait()
     windows.switchWindowByDict('driggsAgentList')
-    wait(2)
+    wait()
     selenBrowser.refresh()
-    wait(2)
+    wait()
     return True
 
 def enterDataForDate(agent, titleAgencies, windows, selenBrowser, startDate, endDate, fullYearRecord):
-    
+
     startDateEntry = startDate.replace("/","")
     endDateEntry = endDate.replace("/","")
 
@@ -112,12 +106,29 @@ def enterDataForDate(agent, titleAgencies, windows, selenBrowser, startDate, end
         windows.switchWindowByDict('driggsAgentList')
         agentName = selenBrowser.find_element_by_xpath('//a[@href]').text
         names = str.split(agentName.lower())
-        lastName = names[len(names) - 1]
-        wait(2)
+        try:
+            lastName = names[len(names) - 1]
+        except IndexError:
+            # Name not found, skip name and return function.
+            windows.switchWindowByDict('driggsAgentList')
+            wait()
+            selenBrowser.find_element_by_name('skipBtn').click()
+            wait()
+            return True
+
+        if lastName == "" or lastName == " ":
+            # Name not found, skip name and return function.
+            windows.switchWindowByDict('driggsAgentList')
+            wait()
+            selenBrowser.find_element_by_name('skipBtn').click()
+            wait()
+            return True
+
+        wait()
         windows.switchWindowByDict('mtaSearch')
-        wait(2)
+        wait()
         selenBrowser.find_element_by_xpath('//input[@value="<<"]').click()
-        wait(2)
+        wait()
         nameField = selenBrowser.find_element_by_name("agentsearch")
         wait()
         nameField.clear()
@@ -155,15 +166,15 @@ def enterDataForDate(agent, titleAgencies, windows, selenBrowser, startDate, end
                     nameFound = True
                     name.click()
                     selenBrowser.find_element_by_name("Right").click()
-                    wait(2)
+                    wait()
                     break
 
         if not nameFound:
             # Name not found, skip name and return function.
             windows.switchWindowByDict('driggsAgentList')
-            wait(2)
+            wait()
             selenBrowser.find_element_by_name('skipBtn').click()
-            wait(2)
+            wait()
             return True
 
 
@@ -174,15 +185,15 @@ def enterDataForDate(agent, titleAgencies, windows, selenBrowser, startDate, end
     windows.switchWindowByDict('mtaData')
     wait(6)
     getData(agent, selenBrowser)
-    wait(2)
+    wait()
     selenBrowser.close()
-    wait(2)
+    wait()
 
     if fullYearRecord:
         windows.switchWindowByDict('driggsAgentList')
-        wait(2)
+        wait()
         selenBrowser.find_element_by_xpath('//a[@href]').click()
-        wait(2)
+        wait()
         windows.updateWindows()
         windows.addWindowToDict(len(windows.windows) - 1, 'agentPageOne')
         windows.switchWindowByDict('agentPageOne')
@@ -194,11 +205,11 @@ def enterDataForDate(agent, titleAgencies, windows, selenBrowser, startDate, end
         windows.switchWindowByDict('agentPageTwo')
         wait(3)
         selenBrowser.find_element_by_name('beg_dt_p').send_keys(startDateEntry)
-        wait(2)
+        wait()
         selenBrowser.find_element_by_name('end_dt_p').send_keys(endDateEntry)
-        wait(2)
+        wait()
         selenBrowser.find_element_by_name('mta_yr_type_p').click()
-        wait(2)
+        wait()
         selenBrowser.find_element_by_name("sub").click()
         wait(4)
     else:
@@ -210,9 +221,9 @@ def enterDataForDate(agent, titleAgencies, windows, selenBrowser, startDate, end
             if dateLinks[i] == lastEntry:
                 dateLinks[i].click()
 
-    wait(2)           
+    wait()
     agencyEntry = selenBrowser.find_element_by_name("title_comp_id_p1")
-    wait(2)
+    wait()
     agencyEntry.click()
     wait()
     agencyEntry.click()
@@ -220,8 +231,8 @@ def enterDataForDate(agent, titleAgencies, windows, selenBrowser, startDate, end
     pasteData(agent, titleAgencies, selenBrowser)
     wait(3)
     selenBrowser.find_element_by_name("sub").click()
-    wait(2)
-            
+    wait()
+
 
 def getNextDates(selenBrowser, startDate, endDate):
     dateLinks = selenBrowser.find_elements_by_xpath('//a[@href]')
@@ -241,7 +252,7 @@ def getData(agent, selenBrowser):
     agent.reset_deal_count()
     NONE = []
     titleNamesHTML = []
- 
+
     try:
         html = selenBrowser.page_source
         soup = BeautifulSoup(html, 'lxml')
@@ -257,11 +268,11 @@ def getData(agent, selenBrowser):
     MS2HTML = soup.find_all('div', attrs={'class':'a143'})
     doubleDipHTML = soup.find_all('div', attrs={'class':'a151'})
     MS3HTML = soup.find_all('div', attrs={'class':'a159'})
-    NONE = str(soup.find_all('a', href = True)) 
- 
-    # Set count equal to the length of the list 
+    NONE = str(soup.find_all('a', href = True))
+
+    # Set count equal to the length of the list
     count = len(titleNamesHTML)
- 
+
     if '<a href="mailto:info@mymta.com">info@mymta.com</a>' in NONE:
         agent.add_deal('none', '1', '1', '1', '1')
         count = 1
@@ -273,65 +284,65 @@ def getData(agent, selenBrowser):
             for i in range(count):
                 currentTitleName = str(titleNamesHTML[i])
                 currentTitleName = currentTitleName[17:-6]
- 
+
                 currentListingSideSTR = str(listingSideHTML[i])
                 currentListingSideSTR = currentListingSideSTR[19:-6]
- 
+
                 currentMS1STR = str(MS1HTML[i])
                 currentMS1STR = currentMS1STR[18:-6]
- 
+
                 currentBuySideSTR = str(buySideHTML[i])
                 currentBuySideSTR = currentBuySideSTR[19:-6]
- 
+
                 currentMS2STR = str(MS2HTML[i])
                 currentMS2STR = currentMS2STR[18:-6]
- 
+
                 currentDoubleDipSTR = str(doubleDipHTML[i])
                 currentDoubleDipSTR = currentDoubleDipSTR[19:-6]
- 
+
                 currentMS3STR = str(MS3HTML[i])
                 currentMS3STR = currentMS3STR[18:-6]
- 
+
                 currentListingSideSTR = currentListingSideSTR.replace(',' , '')
                 currentBuySideSTR = currentBuySideSTR.replace(',' , '')
                 currentDoubleDipSTR = currentDoubleDipSTR.replace(',' , '')
- 
+
                 currentListingSide = int(currentListingSideSTR)
                 currentMS1 = int(currentMS1STR)
                 currentBuySide = int(currentBuySideSTR)
                 currentMS2 = int(currentMS2STR)
                 currentDoubleDip = int(currentDoubleDipSTR)
                 currentMS3 = int(currentMS3STR)
- 
+
                 currentMS1 += currentMS3
                 currentMS1STR = str(currentMS1)
- 
+
                 currentMS2 += currentMS3
                 currentMS2STR = str(currentMS2)
- 
+
                 if currentDoubleDip > 0 and currentListingSide == 0:
- 
+
                     currentListingSide = currentDoubleDip
                     currentListingSideSTR = str(currentListingSide)
- 
+
                 if currentDoubleDip > 0 and currentBuySide == 0:
- 
+
                     currentBuySide = currentDoubleDip
                     currentBuySideSTR = str(currentBuySide)
- 
+
                 agent.add_deal(currentTitleName[0:17].lower(), currentListingSideSTR.lower(), currentMS1STR.lower(), currentBuySideSTR.lower(), currentMS2STR.lower())
                 time.sleep(0.01)
- 
+
 def pasteData(agent, titleAgencies, selenBrowser):
     MAX_NUM_DEALS = 30
 
     numDeals = agent.deal_count() if agent.deal_count() <= MAX_NUM_DEALS else MAX_NUM_DEALS
     for x in range(numDeals):
-        
+
         matchFound = True
         arrowKeyCount = 0
         shortDuplicate = 0
-        fullDuplicate = 0 
+        fullDuplicate = 0
         occurences = 0
         nameLength = 17
 
@@ -384,7 +395,7 @@ def pasteData(agent, titleAgencies, selenBrowser):
             wait(0.3)
 
             for i in range(abs(arrowKeyCount)):
- 
+
                 keyboard.press_and_release('down arrow')
                 wait(0.3)
 
--- main.py
+++ main.py
@@ -8,16 +8,22 @@ def main():
     selenBrowser = webdriver.Chrome('./chromedriver')
     windows = browser_windows.BrowserWindows(selenBrowser)
 
-    NUMBER_OF_NAMES = 50
+    functions.setDates("01/31/2021", "01/31/2022")
     functions.getAgencies(user, titleAgencies, windows, selenBrowser)
-    nameCountAdjusted = NUMBER_OF_NAMES
-    for nameNumber in range(NUMBER_OF_NAMES):
+
+    NUMBER_OF_NAMES = 200
+    namesCompleted = 0
+    namesSkipped = 0
+
+    for i in range(NUMBER_OF_NAMES):
         dataEntered = functions.enterData(agent, titleAgencies, windows, selenBrowser)
         if dataEntered:
-            print("Name Completed: #" + str(nameNumber +1))
+            namesCompleted += 1
         else:
-            print("Name Skipped: #" + str(nameNumber + 1))
-            nameCountAdjusted -= 1
-    print("Program Complete. Names Completed: " + str(nameCountAdjusted))
+            namesSkipped += 1
+
+        print("Names Completed: " + namesCompleted + ", Names Skipped: " + namesSkipped)
+
+    print("Program Complete. Names Completed: " + str(namesCompleted))
 
 main()
